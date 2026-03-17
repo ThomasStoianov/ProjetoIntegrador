7681031d51ac22dafb182afa1b07dcbfaaf7c0ce
THOMÁS GAJEWSKI STOIANOV <54532962838@info.local>
2026-03-17 20:07:55 -0300

    Processamento dos alunos feito

---
+++ main.py
@@ -0,0 +1,15 @@
+from processamento import processar_alunos
+
+dados = [
+    ("João", [8, 7, 9]), 
+    ("Maria", [5, 6, 4]), 
+    ("Pedro", [10, 9, 10]),  
+    ("Ana", []),               
+    ("Carlos", [7, "erro", 8]) 
+]
+
+alunos, recuperacao, top_student = processar_alunos(dados)
+
+print("Alunos:", alunos)
+print("Recuperação:", recuperacao)
+print("Top Student:", top_student)
